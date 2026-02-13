5173028afd35a5c0945f76e3fddc46fcad7168e3
niedev <contact.niedev@gmail.com>
2026-02-13 17:54:57 +0100

    Implemented missing dirs creation and transated comments in enhancer_madlad

--- enhancer_madlad.py
+++ enhancer_madlad.py
@@ -1,6 +1,7 @@
 import copy
 from operator import contains
 from os import PathLike
+import os
 from pathlib import Path
 import pprint
 import huggingface_hub
@@ -19,12 +20,13 @@ import optimum.exporters.onnx
 from onnx import TensorProto, numpy_helper
 import numpy as np
 from datasets import load_dataset, concatenate_datasets, DatasetDict, Dataset
+from optimum.exporters.onnx import main_export
 
 
 en_text = "Also unlike 2014, there aren’t nearly as many loopholes. You can’t just buy a 150-watt incandescent or a three-way bulb — the ban covers any normal bulb that generates less than 45 lumens per watt, which pretty much rules out both incandescent and halogen tech in their entirety."
 
 
-def create_madlad_final_model(execute_model: bool):
+def create_madlad_final_model(execute_model: bool = False):
     convert_madlad_cache_optimum()
     create_madlad_cache_initializer("onnx/Madlad/Optimum_Cache_Optimized/decoder_model.onnx", "onnx/Madlad/Optimum_Cache_Optimized/cache_initializer.onnx")
     create_madlad_embed("onnx/Madlad/Optimum_Cache_Optimized/decoder_with_past_model.onnx", "onnx/Madlad/Optimum_Cache_Optimized/ReducedRam/madlad_embed.onnx")
@@ -48,19 +50,25 @@ def create_madlad_final_model(execute_model: bool):
 
 
 def convert_madlad_cache_optimum():
-    # metodo per esportare Madlad in formato Onnx con optimum e kv cache
+    # Method to export Madlad to Onnx format with optimum and kv cache
     model_name = 'jbochi/madlad400-3b-mt'
     save_directory = "onnx/Madlad/Optimum_Cache_Optimized"
+
+    os.makedirs(save_directory, exist_ok=True)
 
     if((not Path(save_directory + "/encoder_model.onnx").is_file()) or (not Path(save_directory + "/decoder_with_past_model.onnx").is_file())):
       model = T5ForConditionalGeneration.from_pretrained(model_name, use_cache=True, attn_implementation="eager")
       model.eval()
-      optimum.exporters.onnx.onnx_export_from_model(model, Path(save_directory), opset=18, optimize="O1")
+      optimum.exporters.onnx.onnx_export_from_model(model, Path(save_directory), opset=18, optimize="O1", no_post_process=True)
+      
 
 
 def create_madlad_cache_initializer(model_path, model_path_out):
+    directory = os.path.dirname(model_path_out)
+    os.makedirs(directory, exist_ok=True)
+
     if(not Path(model_path_out).is_file()):
-        #preleviamo il decoder con kv_cache senza past, per estrarre le componenti da inserire nell initializer.
+        # We extract the decoder with kv_cache without past, to extract the components to be inserted into the initializer.
         model = onnx.load_model(model_path)
 
         graph = model.graph
@@ -68,13 +76,13 @@ def create_madlad_cache_initializer(model_path, model_path_out):
         nodes = graph.node
 
 
-        # creiamo un dizionario che associa al nome di ogni initializer del grafico le sue informazioni
+        # we create a dictionary that associates the name of each initializer in the graph with its information
         initializers_dict = {}
         for initializer in initializers:
             initializers_dict[initializer.name] = initializer
 
-        # creiamo una lista contenente la matrice (initializer) di ogni MatMul che useremo nel kv_generator
-        # e una lista contenente tutte le MatMul che useremo nel kv_generator
+        # Let's create a list containing the matrix (initializer) of each MatMul we'll use in the kv_generator
+        # and a list containing all the MatMuls we'll use in the kv_generator
         inputs_list = []
         nodes_list = []
         for node in nodes:
@@ -94,36 +102,36 @@ def create_madlad_cache_initializer(model_path, model_path_out):
         while(node_index < len(nodes_list)):   #for node in nodes
             node = nodes_list[node_index]
 
-            #for per trovare il numero del block di questo nodo (serve per scrivere il nome dell'output del transpose)
+            # cycle to find the block number of this node (this is used to write the transpose output name)
             index = 0
             for i in range(32):
                 if("block."+str(i) in node.name):
                     index = i
-            #capiamo se il nodo corrente `e un key o value
+            # we understand if the current node is a key or value
             if("/k/" in node.name):
                 isKey = True
             else:
                 isKey = False
 
-            #creaiamo e inseriamo il reshape in node_list
-            shape_initializer = onnx.helper.make_tensor(   #si crea una tensore che rappresenta la forma del reshape
+            # create and insert the reshape into node_list
+            shape_initializer = onnx.helper.make_tensor(   # a tensor is created that represents the shape of the reshape
                                     name="shape_"+str(count_output_shape),
                                     data_type=onnx.TensorProto.INT64,
                                     dims=[4],
-                                    vals=[1, -1, 16, 128]   #si usa una batch size fissa di 1 cosi da poter usare -1 al posto della dimensione dinamica (dato che non si puo inserire una dimensione dinamica in un reshape)
+                                    vals=[1, -1, 16, 128]   # a fixed batch size of 1 is used so that -1 can be used instead of the dynamic size (since you can't put a dynamic size in a reshape)
             )
-            inputs_list.extend([shape_initializer])  #si aggiunge agli initializer del grafico
-            reshape = onnx.helper.make_node(   #si crea il reshape
+            inputs_list.extend([shape_initializer])  # It is added to the graph initializers
+            reshape = onnx.helper.make_node(   # the reshape is created
                                     name="Reshape_custom_"+str(count_output_shape),
                                     op_type="Reshape",
                                     inputs=[node.output[0], "shape_"+str(count_output_shape)],
                                     outputs=["Reshape_custom_output_"+str(count_output_shape)],
                                 )
-            nodes_list.insert(node_index+1, reshape)   #si aggiunge ai nodi del grafico
+            nodes_list.insert(node_index+1, reshape)   # is added to the graph nodes
             node_index = node_index+1
 
-            #creaiamo e inseriamo il transpose in node_list
-            #perm = onnx.helper.make_attribute(   #si crea un attributo che rappresenta la perm del transpose
+            # create and insert the transpose into node_list
+            #perm = onnx.helper.make_attribute(   # an attribute is created that represents the perm of the transpose
             #        key="perm",
             #        value=[0, 2, 1, 3],
             #        attr_type=AttributeType.INTS
@@ -133,14 +141,14 @@ def create_madlad_cache_initializer(model_path, model_path_out):
                 output_name = "present."+str(index)+".encoder.key"
             else:
                 output_name = "present."+str(index)+".encoder.value"
-            transpose = onnx.helper.make_node(   #si crea il transpose
+            transpose = onnx.helper.make_node(   # the transpose is created
                                     name="Transpose_custom_"+str(count_output_shape),
                                     op_type="Transpose",
                                     inputs=["Reshape_custom_output_"+str(count_output_shape)],
                                     outputs=[output_name],
                                     **perm
                                 )
-            nodes_list.insert(node_index+1, transpose)   #si aggiunge ai nodi del grafico
+            nodes_list.insert(node_index+1, transpose)   # it is added to the graph nodes
             node_index = node_index+1
 
 
@@ -183,13 +191,16 @@ def create_madlad_cache_initializer(model_path, model_path_out):
 
 
 def create_madlad_embed(decoder_path: PathLike, output_path: PathLike):
+    directory = os.path.dirname(output_path)
+    os.makedirs(directory, exist_ok=True)
+
     if(not Path(output_path).is_file()):
         model = onnx.load(decoder_path)
         graph = model.graph
         initializers = graph.initializer
         nodes = graph.node
 
-        # creiamo un dizionario che associa al nome di ogni initializer del grafico le sue informazioni
+        # we create a dictionary that associates the name of each initializer in the graph with its information
         initializers_dict = {}
         for initializer in initializers:
             initializers_dict[initializer.name] = initializer
@@ -200,7 +211,7 @@ def create_madlad_embed(decoder_path: PathLike, output_path: PathLike):
 
         embed_nodes = []
         embed_initializers = []
-        # aggiungiamo i nodi e gli initializers richiesti per nllb_embed
+        # Let's add the required nodes and initializers for madlad_embed
         for node in nodes:
             if(node.name == "/decoder/Reshape"):
                 embed_nodes.append(node)
@@ -213,7 +224,7 @@ def create_madlad_embed(decoder_path: PathLike, output_path: PathLike):
         embed_initializers.append(initializers_dict["shared.weight"])
 
 
-        # Creazione del grafico nllb_embed
+        # Creating the madlad_embed chart
         #create inputs
         input_ids = onnx.helper.make_tensor_value_info("input_ids",
                                                             onnx.TensorProto.INT64,
@@ -242,6 +253,11 @@ def create_madlad_embed(decoder_path: PathLike, output_path: PathLike):
 
 
 def adapt_madlad_to_embed(encoder_path: PathLike, decoder_path: PathLike, encoder_path_out: PathLike, decoder_path_out: PathLike):
+    encoderDirectory = os.path.dirname(encoder_path_out)
+    decoderDirectory = os.path.dirname(decoder_path_out)
+    os.makedirs(encoderDirectory, exist_ok=True)
+    os.makedirs(decoderDirectory, exist_ok=True)
+
     if((not Path(encoder_path_out).is_file()) or (not Path(decoder_path_out).is_file())):
         encoder_model = onnx.load(encoder_path)
         encoder_graph = encoder_model.graph
@@ -253,16 +269,16 @@ def adapt_madlad_to_embed(encoder_path: PathLike, decoder_path: PathLike, encode
         decoder_initializers = decoder_graph.initializer
         decoder_nodes = decoder_graph.node
 
-        # creiamo un dizionario che associa al nome di ogni initializer del grafico dell' encoder le sue informazioni
+        # we create a dictionary that associates the name of each initializer of the encoder graph with its information
         encoder_initializers_dict = {}
         for initializer in encoder_initializers:
             encoder_initializers_dict[initializer.name] = initializer
-        # creiamo un dizionario che associa al nome di ogni initializer del grafico del decoder le sue informazioni
+        # we create a dictionary that associates the name of each initializer of the decoder graph with its information
         decoder_initializers_dict = {}
         for initializer in decoder_initializers:
             decoder_initializers_dict[initializer.name] = initializer
 
-        # creiamo un dizionario che associa a ogni nome di ogni input del grafico dell encoder una lista contenente tutti i nodi (op) che possiedono quell'input
+        # we create a dictionary that associates to each name of each input of the encoder graph a list containing all the nodes (op) that have that input
         encoder_inputs_dict = {}
         for node in encoder_nodes:
             for input in node.input:
@@ -270,7 +286,7 @@ def adapt_madlad_to_embed(encoder_path: PathLike, decoder_path: PathLike, encode
                     encoder_inputs_dict[input] = [node]
                 elif(node not in encoder_inputs_dict[input]):
                     encoder_inputs_dict[input].append(node)
-        # creiamo un dizionario che associa a ogni nome di ogni input del grafico dell encoder una lista contenente tutti i nodi (op) che possiedono quell'input
+        # we create a dictionary that associates to each name of each input of the encoder graph a list containing all the nodes (op) that have that input
         decoder_inputs_dict = {}
         for node in decoder_nodes:
             for input in node.input:
@@ -284,7 +300,7 @@ def adapt_madlad_to_embed(encoder_path: PathLike, decoder_path: PathLike, encode
         #del graph
         #del initializers
 
-        #rimuoviamo dall'encoder i nodi esportati in madlad_embed e rinominiamo l'input degli altri nodi di conseguenza
+        # We remove the nodes exported to madlad_embed from the encoder and rename the input of the other nodes accordingly
         output = "/embed_tokens/Gather_output_0"
         if(output in encoder_inputs_dict):
             for node2 in encoder_inputs_dict[output]:
@@ -302,7 +318,7 @@ def adapt_madlad_to_embed(encoder_path: PathLike, decoder_path: PathLike, encode
             if(node.name == "/embed_tokens/Gather"):
                 encoder_nodes.remove(node)
 
-        #rimuoviamo dal decoder i nodi esportati in madlad_embed e rinominiamo l'input degli altri nodi di conseguenza
+        # We remove the nodes exported to madlad_embed from the decoder and rename the input of the other nodes accordingly
         output = "/decoder/shared/Gather_output_0"
         if(output in decoder_inputs_dict):
             for node2 in decoder_inputs_dict[output]:
@@ -320,21 +336,21 @@ def adapt_madlad_to_embed(encoder_path: PathLike, decoder_path: PathLike, encode
             if(node.name == "/decoder/shared/Gather"):
                 decoder_nodes.remove(node)
 
-        #modifichiamo gli input dell'encoder e rimuoviamo i pesi dell'embed
+        # We modify the encoder inputs and remove the embed weights
         embed_matrix = onnx.helper.make_tensor_value_info("embed_matrix",
                                                             onnx.TensorProto.FLOAT,
                                                             ["batch_size", "sequence_length", 1024])
         encoder_graph.input.insert(0, embed_matrix)
         encoder_graph.initializer.remove(encoder_initializers_dict["embed_tokens.weight"])
 
-        #modifichiamo gli input e gli output del decoder e rimuoviamo i pesi dell'embed
+        # We modify the decoder inputs and outputs and remove the embed weights
         embed_matrix = onnx.helper.make_tensor_value_info("embed_matrix",
                                                             onnx.TensorProto.FLOAT,
                                                             ["batch_size", "sequence_length", 1024])
         decoder_graph.input.insert(0, embed_matrix)
         decoder_graph.initializer.remove(decoder_initializers_dict["shared.weight"])
 
-        #salviamo l' encoder e il decoder aggiornati
+        # save the updated encoder and decoder
         onnx.save_model(encoder_model, encoder_path_out, save_as_external_data=True)
         onnx.shape_inference.infer_shapes_path(encoder_path_out, encoder_path_out)
 
@@ -360,12 +376,13 @@ def quantize_madlad_4bit(qdq = False, quality=False, outputFolder = "onnx/Madlad
 
     quant_config_hqq = matmul_nbits_quantizer.HQQWeightOnlyQuantConfig()  #op_types_to_quantize={"MatMul", "Gather"} (Gather non è supportato con HQQ)
 
+    os.makedirs(outputFolder, exist_ok=True)
 
     #quantization of encoder
     model_fp32_path="onnx/Madlad/Optimum_Cache_Optimized/ReducedRam/encoder_model.onnx"
     model_int4_path=outputFolder + "madlad_encoder_4bit.onnx"
     if(not Path(model_int4_path).is_file()):
-        _quantize_weight_only(model_fp32_path, model_int4_path, quant_config, get_DenseReluDense_nodes(model_fp32_path) if quality else None, accuracy_level, True)
+        _quantize_weight_only(model_fp32_path, model_int4_path, quant_config, get_DenseReluDense_nodes(model_fp32_path) if quality else None, accuracy_level, False)
         if(quality):
             _quantize_weight_only(model_int4_path, model_int4_path, quant_config_int8)
         set_model_matmul_accuracy_level(model_int4_path, model_int4_path, accuracy_level)
@@ -376,7 +393,7 @@ def quantize_madlad_4bit(qdq = False, quality=False, outputFolder = "onnx/Madlad
     model_int4_path=outputFolder + "madlad_decoder_4bit.onnx"
 
     if(not Path(model_int4_path).is_file()):
-        _quantize_weight_only(model_fp32_path, model_int4_path, quant_config, get_DenseReluDense_nodes(model_fp32_path) if quality else None, accuracy_level, True)
+        _quantize_weight_only(model_fp32_path, model_int4_path, quant_config, get_DenseReluDense_nodes(model_fp32_path) if quality else None, accuracy_level, False)
         if(quality):
             _quantize_weight_only(model_int4_path, model_int4_path, quant_config_int8)
         set_model_matmul_accuracy_level(model_int4_path, model_int4_path, accuracy_level)
@@ -397,6 +414,8 @@ def quantize_madlad_4bit(qdq = False, quality=False, outputFolder = "onnx/Madlad
 
     if(not Path(model_int8_path).is_file()):
         _quantize_dynamic_int8(model_fp32_path, model_int8_path)
+    
+    print("\n\nFinal models saved in "+outputFolder)
 
 
 
@@ -436,6 +455,7 @@ def quantize_madlad_8bit(quality = False, weightOnly = False, qdq = False, outpu
 
     quant_config_hqq = matmul_nbits_quantizer.HQQWeightOnlyQuantConfig()  #op_types_to_quantize={"MatMul", "Gather"} (Gather non è supportato con HQQ)
 
+    os.makedirs(outputFolder, exist_ok=True)
 
     #quantization of encoder
     model_fp32_path="onnx/Madlad/Optimum_Cache_Optimized/ReducedRam/encoder_model.onnx"
@@ -484,12 +504,15 @@ def quantize_madlad_8bit(quality = False, weightOnly = False, qdq = False, outpu
 
 
 def set_model_matmul_accuracy_level(input_path: PathLike, output_path: PathLike, accuracy_level: int):
+    directory = os.path.dirname(output_path)
+    os.makedirs(directory, exist_ok=True)
+
     if(output_path == input_path or (not Path(output_path).is_file())):
         model = onnx.load(input_path)
         graph = model.graph
         nodes = graph.node
 
-        # aggiungiamo l'attributo accuracy_level a tutti i nodi MatMulNBits del modello
+        # We add the accuracy_level attribute to all MatMulNBits nodes in the model
         for node in nodes:
             if(("/MatMul_Q4" in node.name) and node.op_type == "MatMulNBits"):
                 index = 0
@@ -509,7 +532,7 @@ def set_model_matmul_accuracy_level(input_path: PathLike, output_path: PathLike,
 
 
 
-def convert_HQQ_model_to_full_int4(input_path: PathLike, output_path: PathLike):
+def convert_HQQ_model_to_full_int4(input_path: PathLike, output_path: PathLike): #it is not used because it has the same quality of default quantization (RTN)
     if(output_path == input_path or (not Path(output_path).is_file())):
         model = onnx.load(input_path)
         graph = model.graph
@@ -609,7 +632,7 @@ def get_DenseReluDense_nodes(path):
 
 
 if __name__ == '__main__':
-    create_madlad_final_model(False)
+    create_madlad_final_model()
     
     '''onnx_execution.compare_models_quality_multi_language(
         initializer_path="onnx/Madlad/Optimum_Cache_Optimized/cache_initializer.onnx",
